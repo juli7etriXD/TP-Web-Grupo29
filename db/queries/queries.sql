9a72e786c6dae5901200839dfd398c1532e33df6
juli7etriXD <jrivero@alumnos.exa.unicen.edu.ar>
2026-09-08 15:19:17 -0300

    (sqlc): ejecucion del sqlc

--- db/queries/queries.sql
+++ db/queries/queries.sql
@@ -2,60 +2,60 @@
 
 --Create
 
--- Create User :one
-INSERT INTO usuario (nombre, apellido, email, password) 
-VALUES ($1, $2, $3, $4) RETURNING *;
+-- name: CreateUser :one
+INSERT INTO usuario (nombre, apellido, email, password, fecha_nacimiento)
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
 
--- Create Libro :one 
+-- name: CreateLibro :one
 INSERT INTO libro (titulo, autor, fecha_publicacion, genero) 
 VALUES ($1, $2, $3, $4) RETURNING *;
 
--- Create Usuario_Libro :one
+-- name: CreateUsuarioLibro :one
 INSERT INTO usuario_libro (usuario_id, libro_id, leido) 
 VALUES ($1, $2, $3) RETURNING *;
 
 -- Read
 
--- Get User by ID :one
+-- name: GetUserByID :one
 SELECT * FROM usuario WHERE id = $1;
 
--- Get Libro by ID :one
+-- name: GetLibroByID :one
 SELECT * FROM libro WHERE id = $1;
 
--- Get all Libros :many
+-- name: GetAllLibros :many
 SELECT * FROM libro;
 
--- Get all Usuarios
+-- name: GetAllUsuarios :many
 SELECT * FROM usuario;
 
--- Get all Libros read by a User :many
+-- name: GetLibrosLeidosPorUsuario :many
 SELECT l.* 
 FROM libro l JOIN usuario_libro ul ON l.id = ul.libro_id
 WHERE ul.usuario_id = $1 AND ul.leido = true;
 
--- Get all User reading a specific Libro :many
+-- name: GetUsuariosPorLibro :many
 SELECT u.*
 FROM usuario u JOIN usuario_libro ul ON u.id = ul.usuario_id
 WHERE ul.libro_id = $1 AND ul.leido = true;
 
 -- Update
 
--- Update User_Libro Status read :exec
+-- name: UpdateEstadoUsuarioLibro :exec
 UPDATE usuario_libro
 SET leido = $1, fecha_lectura = CURRENT_TIMESTAMP
 WHERE usuario_id = $2 AND libro_id = $3;
 
 -- Delete
 
--- Delete User :exec
+-- name: DeleteUser :exec
 DELETE FROM usuario
 WHERE id = $1;
 
--- Delete Libro :exec
+-- name: DeleteLibro :exec
 DELETE FROM libro
 WHERE id = $1;
 
--- Delete User_Libro :exec
+-- name: DeleteUsuarioLibro :exec
 DELETE FROM usuario_libro
 WHERE usuario_id = $1 AND libro_id = $2;
 
